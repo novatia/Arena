8bfb240e9faba10e292f3ad9d8c9e8692dd693da
novatia <teachers>
2023-07-03 17:52:07 +0200

    fixed LG namespace

--- Assets/Classroom/072023/GrassoLorenzo/Scripts/MoveTowards.cs
+++ Assets/Classroom/072023/GrassoLorenzo/Scripts/MoveTowards.cs
@@ -4,30 +4,33 @@ using System.Collections.Generic;
 
 namespace MBTExample
 {
-    [AddComponentMenu("")]
-    [MBTNode("Grasso Lorenzo/MoveToTheEnemy")]
-    public class MoveTowards : Leaf
+    namespace LG
     {
-        bool isChasing;
-        LGIArenaPlayer m_Player;
-
-        private void Start()
+        [AddComponentMenu("")]
+        [MBTNode("Grasso Lorenzo/MoveToTheEnemy")]
+        public class MoveTowards : Leaf
         {
-            m_Player = GetComponentInParent<LGIArenaPlayer>();
+            bool isChasing;
+            LGIArenaPlayer m_Player;
+
+            private void Start()
+            {
+                m_Player = GetComponentInParent<LGIArenaPlayer>();
+            }
+
+            public override NodeResult Execute()
+            {
+                if (m_Player.Enemies.Count == 0) return NodeResult.failure;
+
+                m_Player.Enemies.Reverse();
+
+                isChasing = m_Player.Destination(m_Player.Enemies[0].transform.position);
+
+                if (!isChasing) return NodeResult.running;
+                else return NodeResult.success;
+
+            }
+
         }
-
-        public override NodeResult Execute()
-        {
-            if(m_Player.Enemies.Count==0) return NodeResult.failure;
-
-            m_Player.Enemies.Reverse();
-
-            isChasing =  m_Player.Destination(m_Player.Enemies[0].transform.position);
-
-            if (!isChasing) return NodeResult.running;
-            else return NodeResult.success;
-           
-        }
-        
     }
 }
