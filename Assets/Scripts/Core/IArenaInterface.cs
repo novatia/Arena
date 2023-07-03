2044d919f107cc8bc8854b53dd302066fb89360a
novatia <teachers>
2023-07-03 14:52:28 +0200

    changed player interface

--- Assets/Scripts/Core/IArenaInterface.cs
+++ Assets/Scripts/Core/IArenaInterface.cs
@@ -2,9 +2,42 @@
 
 public interface IArenaInterface
 {
-    public void TakeDamage();
-    protected bool Block();
-    protected bool Attack(IArenaInterface target);
-    protected bool MoveTo(Vector3 offset);
-    public Transform GetTransform();
+    /// <summary>
+    /// Set player in blocking position, speed and damage is halved
+    /// </summary>
+    /// <returns></returns>
+    public bool Block();
+
+    /// <summary>
+    /// Try to give damage to target
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public bool Attack(IArenaInterface target);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="offset"></param>
+    /// <returns></returns>
+    public bool MoveTo(Vector3 offset);
+
+    /// <summary>
+    /// Jump.
+    /// </summary>
+    /// <returns></returns>
+    public bool Jump();
+
+
+    /// <summary>
+    /// DO NOT OVERRIDE
+    /// </summary>
+    /// <returns></returns>
+    protected Transform GetTransform();
+
+    /// <summary>
+    /// DO NOT OVERRIDE
+    /// </summary>
+    /// <returns></returns>
+    protected void TakeDamage();
 }
